912d1a026aa75bdfd8a0d81162f3d85bf815a678
wotmshuaisi <16408269+wotmshuaisi@users.noreply.github.com>
2019-02-03 23:34:52 +0800

    add login logout api interfaces

--- beplaform/api/viewsets/users.py
+++ beplaform/api/viewsets/users.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User, Group
+from django.contrib.auth.backends import ModelBackend as DjangoModelBackend
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login, logout
 from rest_framework.viewsets import ViewSet
 from rest_framework import status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 
@@ -25,3 +29,19 @@ class UserViewset(ViewSet):
         user.groups.add(Group.objects.filter(id=1).first())
         user.save()
         return Response({"status": "success", "response": "User Successfully Created"}, status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=['post', ], )
+    def login(self, request):
+        print(request.data)
+        user = DjangoModelBackend().authenticate(request, username=request.data.get(
+            'email'), password=request.data.get('password'))
+        if user != None:
+            login(request, user)
+            return Response({"status": True})
+        else:
+            return Response({"status": False})
+
+    @action(detail=False, methods=['get', ], )
+    def logout(self, request):
+        logout(request)
+        return Response({"status": True})
--- bookexchange/src/app/djangorest.service.ts
+++ bookexchange/src/app/djangorest.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DjangorestService {
+  defaultHeaders: HttpHeaders = new HttpHeaders({
+    'Content-Type': 'application/json',
+  });
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  register(email: string, password: string): Observable<any> {
+    return this.http.post('/api/users/', { email: email, password: password }, { headers: this.defaultHeaders });
+  }
+
+  // register(email: string, password: string): Observable<any> {
+  //   return this.http.post('/api/users/', { email: email, password: password }, { headers: this.defaultHeaders });
+  // }
+
 }
